6933b619d3cdb489cf221a30288a5864a826409e
ravikgcp <93998612+ravikgcp@users.noreply.github.com>
2022-07-12 13:06:28 +0530

    Create CMS.py

---
+++ CMS.py
@@ -0,0 +1,38 @@
+import psycopg2
+import pandas as pd
+
+l1 = []
+columns=("Id", "Author", "Published_On", "Blog_Text", "Created_On")
+
+host = "lkpgsql001.postgres.database.azure.com"
+dbname = "firstdb"
+user = "pdadmin@lkpgsql001"
+password = "Azure$2022"
+sslmode  = "require"
+conn_string = "host={0} user={1} dbname={2} password={3} sslmode={4}".format(host, user, dbname, password, sslmode)
+try:
+    conn = psycopg2.connect(conn_string)
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM CMS.BLOGS')
+
+    print(columns)
+    for record in cursor:
+        l1.append(record)
+        print(record)
+
+    for i in l1:
+        tempdf = pd.DataFrame(columns=columns)
+        tempdf.loc[0] = i
+        path = r'C:\Users\Ravikumar\Desktop\CMS\\'+tempdf['Author'][0]+'-'+str(tempdf['Created_On'][0].strftime("%B"))
+        if i[2] == None:
+            path+='-Draft-blogs.csv'
+        else:
+            path+='-Published-blogs.csv'
+        tempdf.to_csv(path,index=False)
+    
+    cursor.close()
+    conn.close()
+
+
+except Exception as e:
+    print(e)
